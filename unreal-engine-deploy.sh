1017b49cddad6632b1afae369b9fbfdb005bfb7c
codetik <codetik@users.noreply.github.com>
2021-07-07 19:58:41 +0100

    Create unreal-engine-deploy.sh

---
+++ unreal-engine-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# (ignore files if already there)
+
+### Deploy Assets
+
+#### Deploy Models
+
+#### Deploy Sound
+
+#### Deploy Music
+
+#### Deploy Sprites/Visuals
+
+#### Deploy Particles
+
+#### Deploy Effects
+
+#### Deploy Video
+
+#### Deploy Materials
+
+#### Deploy 
+
+### Deploy Source
+
+### Deploy Config
+
+### Deploy 
