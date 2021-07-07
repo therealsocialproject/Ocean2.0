e83456a9322b9b196189f127fce774062bec69d0
codetik <codetik@users.noreply.github.com>
2021-07-07 19:46:55 +0100

    Add files via upload

---
+++ src/Ocean2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Ocean2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCEAN2_API AOcean2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
