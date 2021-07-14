7c143dbff77752ebee55ae4660a9a8e4af8ef6ae
codetik <codetik@users.noreply.github.com>
2021-07-14 15:54:00 +0100

    Update unreal-engine-deploy.sh
    Not finished.

--- unreal-engine-deploy.sh
+++ unreal-engine-deploy.sh
@@ -2,15 +2,20 @@
 
 # (ignore files if already there)
 
+DEPLOYMENT_PATH="/"
+
 ### Deploy Config
+cp "conf/"* $DEPLOYMENT_PATH/Config
 
 ### Deploy Source
+cp "src/"* $DEPLOYMENT_PATH/Private
+cp "include/"* $DEPLOYMENT_PATH/Classes
 
 ### Deploy Shaders
+cp "shader/"* $DEPLOYMENT_PATH/Shader
 
 ### Deploy Assets
-
-
+cp "asset/"* $DEPLOYMENT_PATH/Asset
 
 #### Deploy Models
 
